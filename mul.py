49def3c5c1711efcf08cdc79a5a1423bb0790c58
shoishizaki <shoishizaki1997@gmail.com>
2019-04-14 15:25:55 +0900

    Write Directionality but this is not finished product

---
+++ mul.py
@@ -0,0 +1,84 @@
+i = 9
+n_1 = []
+n_2 = []
+n_3 = []
+n_4 = []
+n_5 = []
+n_6 = []
+n_7 = []
+n_8 = []
+n_9 = []
+
+for number in range(1,i + 1):
+    n_1.append(number)
+
+for h_1 in n_1:
+    print(h_1,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_2.append(number * 2)
+
+for h_2 in n_2:
+    print(h_2,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_3.append(number * 3)
+
+for h_3 in n_3:
+    print(h_3,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_4.append(number * 4)
+
+for h_4 in n_4:
+    print(h_4,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_5.append(number * 5)
+
+for h_5 in n_5:
+    print(h_5,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_6.append(number * 6)
+
+for h_6 in n_5:
+    print(h_6,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_7.append(number * 7)
+
+for h_7 in n_7:
+    print(h_7,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_8.append(number * 8)
+
+for h_8 in n_8:
+    print(h_8,end=' ')
+
+print()
+
+for number in range(1,i + 1):
+    n_9.append(number * 9)
+
+for h_9 in n_9:
+    print(h_9,end=' ')
+
+
+    
+
