80b8023bbd6ef15aaa55749b3d9a527efbae0035
shoishizaki <shoishizaki1997@gmail.com>
2019-04-14 16:36:38 +0900

    Complete Apr14’s homework

---
+++ mul2.py
@@ -0,0 +1,85 @@
+i = 9
+input_number = int(input())
+n_1 = []
+n_2 = []
+n_3 = []
+n_4 = []
+n_5 = []
+n_6 = []
+n_7 = []
+n_8 = []
+n_9 = []
+
+for number in range(1,i + 1):
+    n_1.append(number)
+    n_2.append(number * 2)
+    n_3.append(number * 3)
+    n_4.append(number * 4)
+    n_5.append(number * 5)
+    n_6.append(number * 6)
+    n_7.append(number * 7)
+    n_8.append(number * 8)
+    n_9.append(number * 9)
+
+
+if input_number == 1:
+    print(*n_1, sep=' ')
+elif input_number == 2:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+elif input_number == 3:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+elif input_number == 4:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+elif input_number == 5:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+    print(*n_5, sep=' ')
+elif input_number == 6:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+    print(*n_5, sep=' ')
+    print(*n_6, sep=' ')
+elif input_number == 7:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+    print(*n_5, sep=' ')
+    print(*n_6, sep=' ')
+    print(*n_7, sep=' ')
+elif input_number == 8:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+    print(*n_5, sep=' ')
+    print(*n_6, sep=' ')
+    print(*n_7, sep=' ')
+    print(*n_8, sep=' ')
+elif input_number == 9:
+    print(*n_1, sep=' ')
+    print(*n_2, sep=' ')
+    print(*n_3, sep=' ')
+    print(*n_4, sep=' ')
+    print(*n_5, sep=' ')
+    print(*n_6, sep=' ')
+    print(*n_7, sep=' ')
+    print(*n_8, sep=' ')
+    print(*n_9, sep=' ')
+else:
+    print('This number is not defined ')
+
+
+
+
+
